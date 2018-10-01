f6abe2d19656f16f6bc62fc2fd72df7a1d13f6bc
Milos Mosovsky <milos.mosovsky@toptal.com>
2018-10-01 19:50:14 +0200

    Parse motivational

--- src/helpers.js
+++ src/helpers.js
@@ -17,6 +17,16 @@ export const TOPICS = {
     vid: '758c0d77bd02e19d921362183e69f763',
     fdd: 'd',
     m: 0
+  },
+  MOTIVATIONAL: {
+    typ: 'topic',
+    langc: 'en',
+    v: '8.4.1:2962932',
+    ab: 'a',
+    id: 't:132622',
+    vid: '7b363d749b4c7c684ace871c8a75f8e6',
+    fdd: 'd',
+    m: 0
   }
 }
 
--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,4 @@
 import parseTopic, { TOPICS } from './helpers'
 
 parseTopic(TOPICS.BUSSINESS, 10, 'business')
+parseTopic(TOPICS.MOTIVATIONAL, 10, 'motivational')
